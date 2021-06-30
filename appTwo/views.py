8b1dc9c1cd235e5d186f5911a03ce854a000dd81
viswanadha <viswapython1@gmail.com>
2021-06-30 13:31:12 +0530

    initial commit

---
+++ appTwo/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from appTwo.models import User
+# Create your views here.
+
+def index(request):
+    return render(request,'apptwo/index.html')
+
+def users(request):
+
+    user_list = User.objects.order_by('first_name')
+    user_dict = {"users":user_list}
+    return render(request,'apptwo/users.html',context=user_dict)
